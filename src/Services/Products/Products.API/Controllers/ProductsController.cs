67749bfb2cb73b4ced9e4df40906d8d856b9a6a8
Emirhan Tasdeviren <emirhantasdeviren@gmail.com>
2023-11-06 23:46:18 +0300

    Implement create product api endpoint

--- src/Services/Products/Products.API/Controllers/ProductsController.cs
+++ src/Services/Products/Products.API/Controllers/ProductsController.cs
@@ -28,7 +28,7 @@ public class ProductsController : ControllerBase
     [HttpGet]
     [Route("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<Product>> ProductById(Guid id)
+    public async Task<ActionResult<Product>> ProductByIdAsync(Guid id)
     {
         var product = await _productCtx.Products.SingleOrDefaultAsync(p => p.Id == id);
         if (product == null)
@@ -37,5 +37,17 @@ public class ProductsController : ControllerBase
         }
 
         return product;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<ActionResult> CreateProductAsync([FromBody] Product product)
+    {
+        _productCtx.Products.Add(product);
+
+        await _productCtx.SaveChangesAsync();
+
+        // return CreatedAtAction(nameof(ProductByIdAsync), new { id = product.Id }, product);
+        return StatusCode(StatusCodes.Status201Created);
     }
 }
