d00965dfd98a567a684b2026395768c182c86ae0
Emirhan Tasdeviren <emirhantasdeviren@gmail.com>
2023-11-28 18:49:40 +0300

    Define shopping cart page

---
+++ src/Web/src/pages/ShoppingCart.jsx
@@ -0,0 +1,13 @@
+import CartList from "../components/CartList/CartList";
+import Summary from "../components/Summary/Summary";
+
+const ShoppingCart = () => {
+    return (
+        <>
+            <CartList />
+            <Summary />
+        </>
+    );
+};
+
+export default ShoppingCart;
