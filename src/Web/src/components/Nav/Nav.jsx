f12d03811f41d19b819f89aacf4ec895c19b8d96
Emirhan Tasdeviren <emirhantasdeviren@gmail.com>
2023-11-28 18:36:34 +0300

    Fix nav layout and svg position

--- src/Web/src/components/Nav/Nav.jsx
+++ src/Web/src/components/Nav/Nav.jsx
@@ -1,7 +1,21 @@
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
 
-const Nav = () => {
+const SignInLink = () => (
+    <li className="sign-in">
+        <NavLink to={"/signin"}>Sign In</NavLink>
+    </li>
+);
+
+const SignUpLink = () => (
+    <li className="sign-up">
+        <NavLink to={"/signup"}>Sign Up</NavLink>
+    </li>
+);
+
+const User = ({ user }) => <li>{user.name}</li>;
+
+const Nav = ({ user }) => {
     return (
         <nav>
             <ul>
@@ -18,7 +32,7 @@ const Nav = () => {
                     <a href="/contact">Contact</a>
                 </li>
                 <li>
-                    <a href="/cart" className="shopping-cart">
+                    <NavLink to={"/cart"}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -33,14 +47,16 @@ const Nav = () => {
                                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
                             />
                         </svg>
-                    </a>
+                    </NavLink>
                 </li>
-                <li className="sign-in">
-                    <NavLink to={"/signin"}>Sign In</NavLink>
-                </li>
-                <li className="sign-up">
-                    <NavLink to={"/signup"}>Sign Up</NavLink>
-                </li>
+                {user ? (
+                    <User user={user}/>
+                ) : (
+                    <>
+                        <SignInLink />
+                        <SignUpLink />
+                    </>
+                )}
             </ul>
         </nav>
     );
