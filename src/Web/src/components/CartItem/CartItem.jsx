e4f62d3b8611e0ffb30b7aad3a05b0fee04ff6e6
Emirhan Tasdeviren <emirhantasdeviren@gmail.com>
2023-11-29 22:36:02 +0300

    Implement shopping cart logic on cart item

--- src/Web/src/components/CartItem/CartItem.jsx
+++ src/Web/src/components/CartItem/CartItem.jsx
@@ -1,16 +1,82 @@
 import "./CartItem.css";
 
-const CartItem = ({ item }) => {
+const _updateQuantity = async (itemId, quantity) => {
+    const cartId = localStorage.getItem("cartId");
+
+    if (cartId) {
+        const body = { quantity };
+        const res = await fetch(
+            "http://localhost:8080/api/v1/carts/" + cartId + "/items/" + itemId,
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+                mode: "cors",
+            },
+        );
+
+        return res;
+    }
+
+    return Response({ error: "Cart does not exists" }, { status: 400 });
+};
+
+const _removeItem = async (itemId) => {
+    const cartId = localStorage.getItem("cartId");
+
+    if (cartId) {
+        const res = await fetch(
+            "http://localhost:8080/api/v1/carts/" + cartId + "/items/" + itemId,
+            {
+                method: "DELETE",
+                mode: "cors",
+            },
+        );
+
+        return res;
+    }
+
+    return Response({ error: "Cart does not exists" }, { status: 400 });
+};
+
+const CartItem = ({ item, updateQuantity, removeItem }) => {
+    const increment = async (event) => {
+        event.preventDefault();
+
+        const res = await _updateQuantity(item.id, item.quantity + 1);
+        if (res.status === 204) {
+            updateQuantity(item.quantity + 1);
+        }
+    };
+
+    const decrement = async (event) => {
+        event.preventDefault();
+
+        if (item.quantity > 1) {
+            const res = await _updateQuantity(item.id, item.quantity - 1);
+            if (res.status === 204) {
+                updateQuantity(item.quantity - 1);
+            }
+        } else {
+            const res = await _removeItem(item.id);
+            if (res.status === 204) {
+                removeItem();
+            }
+        }
+    };
+
     return (
         <div className="cart-item">
-            <img src="https://placehold.co/306x204" />
+            <img src={item.imagePath} />
             <div className="cart-item-info">
-                <h3 className="item-title">Long Long Long Item Name</h3>
-                <span className="price">Price</span>
+                <h3 className="item-title">{item.name}</h3>
+                <span className="price">{item.price + "₺"}</span>
                 <div>
-                    <button>-</button>
-                    <span>1</span>
-                    <button>+</button>
+                    <button onClick={decrement}>-</button>
+                    <span>{item.quantity}</span>
+                    <button onClick={increment}>+</button>
                 </div>
             </div>
         </div>
