744ab52d7b32d915748bba2b982b340d8abc843a
Emirhan Tasdeviren <emirhantasdeviren@gmail.com>
2023-11-29 22:40:43 +0300

    Implement shopping cart logic on summary

--- src/Web/src/components/Summary/Summary.jsx
+++ src/Web/src/components/Summary/Summary.jsx
@@ -1,7 +1,7 @@
 import "./Summary.css";
 
 const Summary = ({ items }) => {
-    const subtotal = items?.reduce((acc, cur) => acc + cur.price, 0) ?? 0.0;
+    const subtotal = items?.reduce((acc, cur) => acc + cur.quantity * cur.price, 0) ?? 0.0;
     const shipping = 0.0;
 
     return (
@@ -9,19 +9,19 @@ const Summary = ({ items }) => {
             <div className="summary-item">
                 <div className="summary-label">Subtotal</div>
                 <div className="summary-value">
-                    {subtotal != 0.0 ? subtotal + "₺" : "31790.90₺"}
+                    {subtotal != 0.0 ? subtotal.toFixed(2) + "₺" : "0₺"}
                 </div>
             </div>
             <div className="summary-item">
                 <div className="summary-label">Shipping</div>
                 <div className="summary-value">
-                    {shipping != 0.0 ? shipping + "₺" : "FREE"}
+                    {shipping != 0.0 ? shipping.toFixed(2) + "₺" : "FREE"}
                 </div>
             </div>
             <div className="summary-total">
                 <div className="summary-total-label">Total</div>
                 <div className="summary-total-value">
-                    {subtotal + shipping + "₺"}
+                    {(subtotal + shipping).toFixed(2) + "₺"}
                 </div>
             </div>
             <button>Check Out</button>
