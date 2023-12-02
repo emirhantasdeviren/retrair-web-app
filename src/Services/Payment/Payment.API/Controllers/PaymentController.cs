c36d9cc496e6e57e8c8470301ab0503800e2e96f
Emirhan Tasdeviren <emirhantasdeviren@gmail.com>
2023-12-02 19:11:29 +0300

    Implement payment api endpoint

--- src/Services/Payment/Payment.API/Controllers/PaymentController.cs
+++ src/Services/Payment/Payment.API/Controllers/PaymentController.cs
@@ -1,5 +1,10 @@
+using System.Diagnostics;
+using AutoMapper;
 using Inveon.eCommerceExample.Payment.API.Models;
+using Inveon.eCommerceExample.Payment.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Inveon.eCommerceExample.Payment.API.Controllers;
 
@@ -7,9 +12,22 @@ namespace Inveon.eCommerceExample.Payment.API.Controllers;
 [Route("/api/v1/payment")]
 public class PaymentController : ControllerBase
 {
+    private readonly IMapper _mapper;
+    private readonly IIyzipayService _iyzipay;
 
-    [HttpGet]
-    public IActionResult Get([FromBody] PaymentDetails payment) {
-        return Ok();
+    public PaymentController(IMapper mapper, IIyzipayService iyzipay)
+    {
+        _mapper = mapper;
+        _iyzipay = iyzipay;
+    }
+
+    [HttpPost]
+    public IActionResult CreatePayment([FromBody] PaymentDetails paymentDetails)
+    {
+        var paymentRequest =
+            _mapper.Map<Iyzipay.Request.CreatePaymentRequest>(paymentDetails);
+        var payment = _iyzipay.CreatePayment(paymentRequest);
+
+        return Ok(payment);
     }
 }
