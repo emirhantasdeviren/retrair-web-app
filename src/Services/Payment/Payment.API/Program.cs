8c52bed91a98dcf9dc4ec2ce5c6b9e14a6428e4a
Emirhan Tasdeviren <emirhantasdeviren@gmail.com>
2023-12-02 19:10:59 +0300

    Inject mapper and iyzipay service to controllers and add newtonsoft support for swagger

--- src/Services/Payment/Payment.API/Program.cs
+++ src/Services/Payment/Payment.API/Program.cs
@@ -1,21 +1,23 @@
-using System.Text.Json.Serialization;
-using Inveon.eCommerceExample.Payment.API.Models;
-using Newtonsoft.Json;
+using AutoMapper;
+using Inveon.eCommerceExample.Payment.API.Mappings;
+using Inveon.eCommerceExample.Payment.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder
-    .Services
-    .AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
+// Add services to the container.
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IIyzipayService, IyzipayService>();
+
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGenNewtonsoftSupport();
 
 var app = builder.Build();
 
@@ -29,70 +31,5 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
-var buyer = new Buyer
-{
-    Id = Guid.NewGuid(),
-    Name = "Emirhan",
-    Surname = "Tasdeviren",
-    City = "Izmir",
-    Country = "Turkey",
-    Email = "emirhantasdeviren@gmail.com",
-    IdentityNumber = "13880321028",
-    RegistrationAddress = "foo",
-};
-
-var paymentCard = new PaymentCard
-{
-    CardHolderName = buyer.Name + " " + buyer.Surname,
-    CardNumber = "5528790000000008",
-    ExpireYear = "2030",
-    ExpireMonth = "12",
-    Cvc = "123",
-};
-
-var shippingAddress = new Address
-{
-    ContactName = "Jane Doe",
-    City = "Istanbul",
-    Country = "Turkey",
-    Details = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
-};
-
-var billingAddress = new Address
-{
-    ContactName = "Jane Doe",
-    City = "Istanbul",
-    Country = "Turkey",
-    Details = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1",
-
-};
-
-var item = new Item
-{
-    Name = "Air Jordan 1 Retro High OG \"Chicago Lost and Found\"",
-    Category = "Sneaker",
-    Id = Guid.NewGuid(),
-    ItemType = ItemType.Physical,
-    Price = 4790.90,
-};
-
-var cartItems = new List<Item>
-{
-    item
-};
-
-var payment = new PaymentDetails
-{
-    Buyer = buyer,
-    BillingAddress = billingAddress,
-    ShippingAddress = shippingAddress,
-    PaymentCard = paymentCard,
-    CartItems = cartItems,
-    Price = 4790.90,
-    PaidPrice = 4790.90,
-};
-
-Console.WriteLine(JsonConvert.SerializeObject(payment, Formatting.Indented));
 
 app.Run();
