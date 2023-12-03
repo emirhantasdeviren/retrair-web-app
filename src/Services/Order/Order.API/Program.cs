e7d61fc757f9809908c0beafc7773962a4993e3a
Emirhan Taşdeviren <emirhan.tasdeviren@outlook.com>
2023-12-03 18:16:36 +0300

    Add services to builder with migrations

--- src/Services/Order/Order.API/Program.cs
+++ src/Services/Order/Order.API/Program.cs
@@ -1,6 +1,14 @@
+using Inveon.eCommerceExample.Order.API.BackgroundServices;
+using Inveon.eCommerceExample.Order.API.Data;
+using Inveon.eCommerceExample.Order.API.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<OrderContext>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddHostedService<EventConsumerService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,5 +27,14 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var _db = scope.ServiceProvider.GetRequiredService<OrderContext>();
+    var pendingMigrations = await _db.Database.GetPendingMigrationsAsync();
+    if (pendingMigrations.Any()) {
+        await _db.Database.MigrateAsync();
+    }
+}
 
 app.Run();
